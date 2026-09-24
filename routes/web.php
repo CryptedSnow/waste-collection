0175b3f112595a5d1be0d49b044b826b7d8a6715
CryptedSnow <iury.fernandes@tuta.io>
2026-09-24 18:56:47 -0300

    Refactor: Changes to web.php about routes.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+
+    $user = Auth::user();
+
+    if (!$user) {
+        return redirect('/user');
+    }
+
+    if ($user->hasAnyRole(['Admin', 'Super Admin'])) {
+        return redirect('/admin');
+    }
+
     return redirect('/user');
 });
